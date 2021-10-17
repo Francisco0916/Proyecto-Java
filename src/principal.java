d07a2a7f795a0ed1c1809f2639d99fecf5da686d
Waldin Castillo <79998745+Francisco0916@users.noreply.github.com>
2021-10-17 16:14:10 -0600

    pantalla

---
+++ src/principal.java
@@ -0,0 +1,6 @@
+public class principal {
+    public static void main(String[] args) {
+        widget v1 = new widget();
+        v1.setVisible(true);
+    }
+}
---
+++ src/widget.java
@@ -0,0 +1,39 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+
+
+public class widget extends JFrame {
+    private Container panel1;
+    private JButton boton1;
+
+
+    public widget(){
+        this.setSize(500,500);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setTitle("BOTON");
+
+        panel1 = getContentPane();
+        panel1.setLayout(null);
+        panel1.setBackground(new Color(153,51,255));
+
+        boton1 = new JButton("CLICK AQUI");
+        boton1.setSize(120,30);
+        boton1.setLocation(200,250);
+        panel1.add(boton1);
+
+        ActionListener Bot = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                formu v2 = new formu();
+                v2.setVisible(true);
+            }
+        };
+
+        boton1.addActionListener(Bot);
+
+
+    }
+
+}
